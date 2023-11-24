072021823be77f6e4982361ead5e9f427c62c5c6
kianafirozian <kianafirozian96@gmail.com>
2023-11-24 18:03:40 +0330

    implementing source and destination changes

--- components/Booking/AutocompleteAddress.tsx
+++ components/Booking/AutocompleteAddress.tsx
@@ -3,39 +3,59 @@ import React, { useEffect, useState } from "react";
 
 export default function AutocompleteAddress() {
   const [source, setSource] = useState<any>("");
+  const [sourceChange, setSourceChange] = useState<any>(false);
+  const [destinationChange, setDestinationChange] = useState<any>(false);
+
   const [addressList, setAddressList] = useState<any>([]);
+  const [destination, setDestination] = useState<any>();
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       if (source) getAddressList();
     }, 1000);
     return () => clearTimeout(delayDebounceFn);
-  }, [source]);
+  }, [source, destination]);
 
   const getAddressList = async () => {
-    const res = await fetch("/api/search-address?q=" + source, {
+    setAddressList([]);
+    const query = sourceChange ? source : destination;
+    const res = await fetch("/api/search-address?q=" + query, {
       headers: {
         "Content-Type": "application/json",
       },
     });
+
     const result = await res.json();
-    setAddressList(result?.searchResult);
+    setAddressList(result);
   };
+
+  console.log("result", addressList);
 
   return (
     <div className="mt-5">
-      <div>
+      <div className="relative">
         <label className="text-gray-400">Where From ?</label>
         <input
           type="text"
           className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:border-yellow-300"
-          onChange={(e) => setSource(e.target.value)}
           value={source}
+          onChange={(e) => {
+            setSource(e.target.value);
+            setSourceChange(true);
+          }}
         />
-        {addressList?.suggestions ? (
-          <div>
+        {addressList?.suggestions && sourceChange ? (
+          <div className="shadow-md p-1 rounded-md absolute w-full bg-white">
             {addressList.suggestions.map((item: any, index: number) => (
-              <h2 key={item.mapbox_id}>
+              <h2
+                className="p-3 hover:bg-gray-100 cursor-pointer"
+                onClick={() => {
+                  setSource(item.place_formatted);
+                  setAddressList([]);
+                  setSourceChange(false);
+                }}
+                key={item.mapbox_id}
+              >
                 {item.full_address || item.place_formatted}
               </h2>
             ))}
@@ -45,10 +65,34 @@ export default function AutocompleteAddress() {
 
       <div className="mt-3">
         <label className="text-gray-400">Where To ?</label>
-        {/* <input
-            type="text"
-            className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:border-yellow-300"
-          /> */}
+        <input
+          type="text"
+          className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:border-yellow-300 text-[14px]"
+          value={destination}
+          onChange={(e) => {
+            setDestination(e.target.value);
+            setDestinationChange(true);
+          }}
+        />
+        {addressList?.suggestions && destinationChange ? (
+          <div
+            className="shadow-md p-1 rounded-md
+            absolute w-full bg-white"
+          >
+            {addressList?.suggestions.map((item: any, index: number) => (
+              <h2
+                key={index}
+                className="p-3 hover:bg-gray-100
+                cursor-pointer"
+                onClick={() => {
+                  onDestinationAddressClick(item);
+                }}
+              >
+                {item.full_address || item.place_formatted}
+              </h2>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
